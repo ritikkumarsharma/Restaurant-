0ad77a90b9b126d19434d8549e26662b839cfe24
Ritik Sharma <126388687+ritikkumarsharma@users.noreply.github.com>
2024-10-05 14:00:39 +0530

    Update Restaurant Management.py

--- Restaurant Management.py
+++ Restaurant Management.py
@@ -1,2 +1,1 @@
 # Restaurant Management -
-This is a simple Food Restaurant Management System developed using Python. It leverages dictionaries to store the menu items and their corresponding prices, and uses loops for various management functionalities such as displaying the menu, taking orders, calculating total bills, and managing inventory.
